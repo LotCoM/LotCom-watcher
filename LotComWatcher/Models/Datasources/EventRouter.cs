a3c76cc5697af82ab3f8400c5ac5d9917c337749
JaredEspo <EspinosaJared22@gmail.com>
2025-06-11 15:02:29 -0400

    Feat: added route logic to get scan outputs into correct files

--- LotComWatcher/Models/Datasources/EventRouter.cs
+++ LotComWatcher/Models/Datasources/EventRouter.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using LotComWatcher.Models.Datatypes;
 
 namespace LotComWatcher.Models.Datasources;
@@ -10,9 +9,27 @@ public static class EventRouter
 
     public static bool Route(ScanEvent ScanEvent)
     {
-        string TablePath = $"{ScanFolder}\\{ScanEvent.Label.Process.FullName}";
-        Console.WriteLine(TablePath);
-        return false;
+        try
+        {
+            // Creating TablePath that points us to the correct folder which is the process name. 
+            string TablePath = $"{ScanFolder}\\{ScanEvent.Label.Process.FullName}.txt";
+            // Creating newEntry which is taking the ScanEvent and formatting it for us through the CSV methods. 
+            string newEntry = ScanEvent.ToCSV();
+            // Creating an array that is reading all the lines through the TablePath file.
+            IEnumerable<string> Lines = File.ReadAllLines(TablePath);
+            // Adding the most recent entry to the bottom of our file.
+            Lines = Lines.Append(newEntry);
+            // Saving our changes 
+            File.WriteAllLines(TablePath, Lines);
+        }
+        // there was an error routing
+        catch
+        {
+            Console.WriteLine($"Could not route ScanEvent: {ScanEvent.ToCSV()}");
+            return false;
+        }
+        // succeeded in routing; return true
+        return true;
     }
 }
 
