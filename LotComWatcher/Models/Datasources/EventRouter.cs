ed643e506ba7efeaf3db980a637d0f66df4b40d1
Mason Ritchason <masonritchason@gmail.com>
2025-06-17 16:05:05 -0400

    refactor(EventRouter): implement basic Validation logic in Route method
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotComWatcher/Models/Datasources/EventRouter.cs
+++ LotComWatcher/Models/Datasources/EventRouter.cs
@@ -1,4 +1,5 @@
 using LotComWatcher.Models.Datatypes;
+using LotComWatcher.Models.Services;
 
 namespace LotComWatcher.Models.Datasources;
 
@@ -13,14 +14,20 @@ public static class EventRouter
         {
             // Creating TablePath that points us to the correct folder which is the process name. 
             string TablePath = $"{ScanFolder}\\{ScanEvent.Label.Process.FullName}.txt";
-            // Creating newEntry which is taking the ScanEvent and formatting it for us through the CSV methods. 
-            string newEntry = ScanEvent.ToCSV();
             // Creating an array that is reading all the lines through the TablePath file.
             IEnumerable<string> Lines = File.ReadAllLines(TablePath);
             // Adding the most recent entry to the bottom of our file.
-            Lines = Lines.Append(newEntry);
-            // Saving our changes 
-            File.WriteAllLines(TablePath, Lines);
+            if (ScanEventInsertionService.ValidateInsertion(ScanEvent, Lines))
+            {
+                string newEntry = ScanEvent.ToCSV();
+                Lines = Lines.Append(newEntry);
+                File.WriteAllLines(TablePath, Lines);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         // there was an error routing
         catch
@@ -32,6 +39,3 @@ public static class EventRouter
         return true;
     }
 }
-
-
-
