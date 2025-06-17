f754b625bf025fd177164674bf7eb88e58575aeb
Mason Ritchason <masonritchason@gmail.com>
2025-06-17 16:51:06 -0400

    feat(Exceptions): implement new exception types
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotComWatcher/Models/Exceptions/InsertionException.cs
@@ -0,0 +1,10 @@
+namespace LotComWatcher.Models.Exceptions;
+
+/// <summary>
+/// Special exception type to indicate an error adding an entry to a Table within the Database.
+/// </summary>
+/// <param name="Message"></param>
+public class InsertionException(string Message, Exception? InnerException = null) : DatabaseException(Message: Message, InnerException: InnerException)
+{
+
+}
