a7ff6a413392d04a512ac7f3a86b526ea4de2503
Mason Ritchason <masonritchason@gmail.com>
2025-06-17 16:51:42 -0400

    refactor(EventRouter): improve exception handling
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotComWatcher/Models/Datasources/EventRouter.cs
+++ LotComWatcher/Models/Datasources/EventRouter.cs
@@ -1,41 +1,63 @@
 using LotComWatcher.Models.Datatypes;
+using LotComWatcher.Models.Exceptions;
 using LotComWatcher.Models.Services;
 
 namespace LotComWatcher.Models.Datasources;
 
-
+/// <summary>
+/// Provides database insertion and routing methods for ScanEvent objects.
+/// </summary>
 public static class EventRouter
 {
-    private static string ScanFolder = "\\\\144.133.122.1\\Lot Control Management\\Database\\data_tables\\scans";
+    /// <summary>
+    /// URI for the "scans" section of the Database, where each Process' scan datatable lives.
+    /// </summary>
+    private static readonly string ScanFolder = "\\\\144.133.122.1\\Lot Control Management\\Database\\data_tables\\scans";
 
+    /// <summary>
+    /// Attempts to route and insert ScanEvent into its appropriate Process datatable.
+    /// </summary>
+    /// <param name="ScanEvent"></param>
+    /// <returns></returns>
     public static bool Route(ScanEvent ScanEvent)
     {
+        // prepare the Table and DatabaseSet
+        string TablePath = "";
+        IEnumerable<string> DatabaseSet; // was Lines
         try
         {
             // Creating TablePath that points us to the correct folder which is the process name. 
-            string TablePath = $"{ScanFolder}\\{ScanEvent.Label.Process.FullName}.txt";
+            TablePath = $"{ScanFolder}\\{ScanEvent.Label.Process.FullName}.txt";
             // Creating an array that is reading all the lines through the TablePath file.
-            IEnumerable<string> Lines = File.ReadAllLines(TablePath);
-            // Adding the most recent entry to the bottom of our file.
-            if (ScanEventInsertionService.ValidateInsertion(ScanEvent, Lines))
-            {
-                string newEntry = ScanEvent.ToCSV();
-                Lines = Lines.Append(newEntry);
-                File.WriteAllLines(TablePath, Lines);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            DatabaseSet = File.ReadAllLines(TablePath);
         }
-        // there was an error routing
-        catch
+        // the file could not be found by the Router
+        catch (FileNotFoundException)
         {
-            Console.WriteLine($"Could not route ScanEvent: {ScanEvent.ToCSV()}");
+            throw new ProcessNameException($"Could not find a table for the Process '{ScanEvent.Label.Process.FullName}'.");
+        }
+        // there was another issue accessing the file
+        catch (SystemException _ex)
+        {
+            throw new DatabaseException
+            (
+                Message: $"Failed to open the file at '{TablePath}' due to the following exception:\n{_ex.Message}.",
+                InnerException: _ex
+            );
+        }
+        // Adding the most recent entry to the bottom of our file.
+        if (ScanEventInsertionService.ValidateInsertion(ScanEvent, DatabaseSet))
+        {
+            // convert the ScanEvent to a string and append it to the end of the DatabaseSet
+            string newEntry = ScanEvent.ToCSV();
+            DatabaseSet = DatabaseSet.Append(newEntry);
+            // save the file with the new entry appended
+            File.WriteAllLines(TablePath, DatabaseSet);
+            return true;
+        }
+        else
+        {
             return false;
         }
-        // succeeded in routing; return true
-        return true;
     }
 }
