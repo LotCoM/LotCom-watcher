883922691cb944e076edba603a5428200d71fd8e
Mason Ritchason <masonritchason@gmail.com>
2025-06-17 17:10:03 -0400

    feat(ScanEventInsertionService): implement GetSerialNumber method
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotComWatcher/Models/Services/ScanEventInsertionService.cs
+++ LotComWatcher/Models/Services/ScanEventInsertionService.cs
@@ -10,27 +10,55 @@ namespace LotComWatcher.Models.Services;
 public static class ScanEventInsertionService
 {
     /// <summary>
+    /// Attempts to retrieve and construct a SerialNumber object from ScanEvent.
+    /// </summary>
+    /// <param name="Event"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    private static SerialNumber GetSerialNumber(ScanEvent Event)
+    {
+        SerialNumber EventNumber;
+        Process EventProcess = Event.Label.Process;
+        // ScanEvent uses a JBK Number
+        if
+        (
+            EventProcess.PassThroughType == PassThroughType.JBK ||
+            EventProcess.Serialization == SerializationMode.JBK
+        )
+        {
+            EventNumber = new SerialNumber(SerializationMode.JBK, Event.Label.Part, Event.Label.VariableFields.JBKNumber!.Literal);
+        }
+        // ScanEvent uses a Lot Number
+        else if
+        (
+            EventProcess.PassThroughType == PassThroughType.Lot ||
+            EventProcess.Serialization == SerializationMode.Lot
+        )
+        {
+            EventNumber = new SerialNumber(SerializationMode.Lot, Event.Label.Part, Event.Label.VariableFields.LotNumber!.Literal);
+        }
+        // some mis-configuration of SerializationMode and PassThroughType caused an error
+        else
+        {
+            throw new ArgumentException($"Cannot retrieve a SerialNumber from the ScanEvent '{Event.ToCSV()}'.");
+        }
+        return EventNumber;
+    }
+
+    /// <summary>
     /// Compares a ScanEvent object's serial data to a raw Database Entry's serial data.
     /// </summary>
     /// <param name="Event"></param>
+    /// <param name="EventNumber"></param>
     /// <param name="Entry"></param>
     /// <returns></returns>
-    private static bool Compare(ScanEvent Event, string Entry)
+    private static bool Compare(ScanEvent Event, SerialNumber EventNumber, string Entry)
     {
         // split Entry to its individual fields and compare to Event's Serial data
         string[] SplitEntry = Entry.Split(",");
         string EntryPartNumber = SplitEntry[3];
         string EntrySerialNumber = SplitEntry[6];
         string EntryDate = SplitEntry[^3];
-        SerialNumber EventNumber;
-        if (Event.Label.Process.Serialization == SerializationMode.JBK)
-        {
-            EventNumber = new SerialNumber(SerializationMode.JBK, Event.Label.Part, Event.Label.VariableFields.JBKNumber!.Literal);
-        }
-        else
-        {
-            EventNumber = new SerialNumber(SerializationMode.Lot, Event.Label.Part, Event.Label.VariableFields.LotNumber!.Literal);
-        }
         // compare the SerialNumbers and Dates
         if
         (
@@ -53,9 +81,12 @@ public static class ScanEventInsertionService
     /// <returns></returns>
     public static bool ValidateInsertion(ScanEvent NewEvent, IEnumerable<string> DatabaseSet)
     {
+        Console.WriteLine(NewEvent.ToCSV());
+        // elicit the Serial Number from the new Scan Event
+        SerialNumber EventNumber = GetSerialNumber(NewEvent);
         // compare the ScanEvent data to each of the DatabaseSet entries
         List<string> Matches = DatabaseSet
-            .Where(x => Compare(NewEvent, x) == true)
+            .Where(x => Compare(NewEvent, EventNumber, x))
             .ToList();
         if (Matches.Count > 0)
         {
