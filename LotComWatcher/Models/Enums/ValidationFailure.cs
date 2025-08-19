b1cf08d569bd39be72801df8f29647f35356f467
Mason Ritchason <masonritchason@gmail.com>
2025-08-19 11:38:50 -0400

    feat: add ValidationFailure enum to control validation failure cases
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotComWatcher/Models/Enums/ValidationFailure.cs
@@ -0,0 +1,15 @@
+namespace LotComWatcher.Models.Enums;
+
+public enum ValidationFailure
+{
+    Accepted,
+    Duplicate,
+    MissingPrevious,
+    InvalidPart,
+    InvalidProcess,
+    JBKNumber,
+    LotNumber,
+    DieNumber,
+    DeburrJBKNumber,
+    HeatNumber
+}
