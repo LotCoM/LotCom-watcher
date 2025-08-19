97037b011aa73312605e5487c7c200f4f5a4cec4
Mason Ritchason <masonritchason@gmail.com>
2025-08-19 09:36:30 -0400

    refactor(Program): remove DI for Service classes (staticized)
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotComWatcher/Program.cs
+++ LotComWatcher/Program.cs
@@ -1,5 +1,4 @@
 using LotComWatcher;
-using LotComWatcher.Models.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 // set the service name
@@ -8,8 +7,6 @@ builder.Services.AddWindowsService(options =>
     options.ServiceName = "LotCom Watcher microservice";
 });
 builder.Services.AddHostedService<Worker>();
-builder.Services.AddSingleton<ReaderService>();
-builder.Services.AddSingleton<NetworkService>();
 
 var host = builder.Build();
 host.Run();
